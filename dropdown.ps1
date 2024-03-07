0a80863e562ba74c6902e60a996ee0ad513a2807
Swaranjitha19 <151921685+Swaranjitha19@users.noreply.github.com>
2024-03-07 14:41:41 +0530

    Create dropdown.ps1

---
+++ dropdown.ps1
@@ -0,0 +1,39 @@
+# Define variables
+$pat = "Your_Personal_Access_Token"
+$organization = "Your_Azure_DevOps_Organization"
+$project = "Your_Azure_DevOps_Project"
+$workItemId = "Your_Work_Item_ID"
+
+# API endpoint for retrieving data from your dynamic source
+$apiEndpoint = "https://api.example.com/data"
+
+# Make a request to the API to retrieve dynamic data
+$dynamicData = Invoke-RestMethod -Uri $apiEndpoint -Headers @{
+    Authorization = "Basic " +
+[Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($pat)"))
+}
+
+# Extract the relevant data for the dropdown list
+$dropdownData = $dynamicData | ForEach-Object { $_.name }
+
+# Convert the dropdown data to JSON format
+$dropdownJson = $dropdownData | ConvertTo-Json
+
+# API endpoint for updating work item field
+$uri = "https://dev.azure.com/$organization/$project/_apis/wit/workitems/$($workItemId)?api-version=6.0"
+
+# Define the request body
+$body = @{
+    "fields" = @{
+        "Your_Work_Item_Field_Name" = $dropdownJson
+    }
+} | ConvertTo-Json
+
+# Invoke REST API to update work item field
+$response = Invoke-RestMethod -Uri $uri -Method PATCH -Headers @{
+    Authorization = "Basic " +
+[Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($pat)"))
+} -ContentType "application/json-patch+json" -Body $body
+
+# Output the response
+$response
